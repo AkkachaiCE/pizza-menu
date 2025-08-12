ab33f123d197970b8b3602c6219bc6f20e45ea5e
AkkachaiCE <akachai1624@gmail.com>
2025-08-12 19:56:19 +0700

    Add live time to footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
   return (
     <div>
-      <h1>Hello Pizza Menu App</h1>
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   )
 }
 
-export function Pizza() {
+function Pizza() {
   return (
     <div>
       <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
       <h2>Pizza spinaci</h2>
       <p>Toomato, mozarella, spinach, and ricotta cheese</p>
     </div>
+  );
+}
+
+function Header() {
+  return <h1>Pizza Menu Application</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+    </div>
   )
+}
+
+function Footer() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+  return <footer>{time} We are Close now!!! My friend</footer>
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App, { Pizza } from './App.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Pizza />
-    <Pizza />
-    <Pizza />
   </StrictMode>,
 )
