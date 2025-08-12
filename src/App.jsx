16812fb884cdfe7a96a3adcb30e24f95e5101c1b
AkkachaiCE <akachai1624@gmail.com>
2025-08-12 19:07:52 +0700

    Add second component and pizza images

--- src/App.jsx
+++ src/App.jsx
@@ -5,3 +5,13 @@ export default function App() {
     </div>
   )
 }
+
+export function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+      <h2>Pizza spinaci</h2>
+      <p>Toomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  )
+}
